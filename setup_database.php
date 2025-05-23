94369a9e6c249b8d35ca43dc5efe796f20a90e47
muhlabibirfani <labibirfani64@gmail.com>
2025-05-23 07:00:26 +0700

    ngubah database bagian orders biar crud bekerja

--- setup_database.php
+++ setup_database.php
@@ -44,8 +44,8 @@ $sql = [
         shipping_code VARCHAR(20) NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (produk_id) REFERENCES produk(id),
-        FOREIGN KEY (user_id) REFERENCES users(id)
+        FOREIGN KEY (produk_id) REFERENCES produk(id) ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;",
 
 
